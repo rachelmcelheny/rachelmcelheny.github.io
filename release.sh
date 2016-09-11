809f8d0bf3140462b50a2a684b9df4360ee3ba66
jessezhang91 <jessezhang91@gmail.com>
2016-09-10 23:21:28 -0400

    release script

---
+++ release.sh
@@ -0,0 +1,51 @@
+#!/bin/sh
+
+while true; do
+	read -p "Ready to release? (yes/no) " yn
+	case $yn in
+		[Yy]*) break;;
+		[Nn]*) exit;;
+		*) echo "Come on...";;
+	esac
+done
+
+if [ "$( git rev-parse --abbrev-ref HEAD )" != "development" ]; then
+	echo "You're not on the development branch. How're you running this script?"
+	exit 1
+fi
+
+if [ ! -z "$( git status --porcelain )" ]; then
+	echo "There's uncommitted code. Commit it!"
+	exit 1
+fi
+
+git remote update
+if [ ! -z "$( git status --porcelain -uno )" ]; then
+	echo "Not even with remote. Pull it!"
+	exit 1
+fi
+
+echo "Okay, starting the release!"
+
+echo -e "\n\n\nPrepping..."
+npm run clean
+
+mkdir dist
+cd dist
+git init
+git remote add origin git@github.com:rachelmcelheny/rachelmcelheny.github.io.git
+git fetch
+git checkout master
+rm -rf *
+
+echo -e "\n\n\nBuilding..."
+npm run build
+
+echo -e "\n\n\nBuilt. Publishing..."
+git add -A .
+git commit -m "Publish site"
+git push
+
+cd ..
+
+echo -e "\n\n\nDone!"
